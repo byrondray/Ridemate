fb38a6b7275ebb8b538fbfba47ebf06a9f36067e
byron dray <byrondray8@gmail.com>
2024-09-21 12:55:20 -0700

    add cahnges to auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,9 @@
-import { checkAndStoreKindeUser } from "@/utils/checkAndStoreKindeUser";
 import Link from "next/link";
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { get } from "http";
-import { getUsers } from "@/services/users";
-const { getUser } = getKindeServerSession();
 
 export default async function Home() {
-  await checkAndStoreKindeUser();
-  const user = await getUser();
-  const users = await getUsers();
   return (
     <div className="container">
-      {users!.map((item) => (
-        <Link key={item.id} href={`/message/${user!.id}/${item.id}`}>
-          <div key={item.id}>
-            <h1>{item.firstName}</h1>
-            <p>{item.email}</p>
-          </div>
-        </Link>
-      ))}
+      <Link href={`/users`}>Find Users</Link>
     </div>
   );
 }
